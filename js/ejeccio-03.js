53a38a8b52afcc49a1d7851ffc0ac6c3541bc33d
BryanX309 <bryanesaucruz2002@gmail.com>
2025-06-09 18:00:22 -0600

    Se agrego el HTML y JS del ejercicio #3

---
+++ js/ejeccio-03.js
@@ -0,0 +1,23 @@
+function multiplicar() {
+  const n1 = parseFloat(document.getElementById("num1").value) || 0;
+  const n2 = parseFloat(document.getElementById("num2").value) || 0;
+  const resultado = n1 * n2;
+
+  document.getElementById("resultado").textContent = 'Multiplicación: '+resultado;
+}
+
+function dividir() {
+  const n1 = parseFloat(document.getElementById("num1").value) || 0;
+  const n2 = parseFloat(document.getElementById("num2").value) || 0;
+  var resultado;
+
+  if(n2 == 0){
+      resultado = 'Error';
+  }else{
+      resultado = n1 / n2;
+  }
+  
+  //console.log(resultado);
+  
+  document.getElementById("resultado").textContent = 'Division: ' + resultado;
+}
