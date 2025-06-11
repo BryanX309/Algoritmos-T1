6085d63f2b86609e24fb79eaf94051d7d523def8
BryanX309 <bryanesaucruz2002@gmail.com>
2025-06-10 22:10:04 -0600

    se agrego el HTML y JS del Ejercicio #8

---
+++ js/ejeccio-08.js
@@ -0,0 +1,10 @@
+function promedio() {
+  const n1 = parseFloat(document.getElementById("num1").value) || 0;
+  const n2 = parseFloat(document.getElementById("num2").value) || 0;
+  const n3 = parseFloat(document.getElementById("num3").value) || 0;
+  
+  let prom = (n1+n2+n3)/3;
+  
+  document.getElementById("prefijo").textContent = 'El Promedio es: ';
+  document.getElementById("resultado").textContent = prom;
+}
